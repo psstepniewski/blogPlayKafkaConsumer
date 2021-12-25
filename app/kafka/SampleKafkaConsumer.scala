d8213520e4a9cfca43e3c62c9864ad596ece37e8
Pawel Stepniewski <pawel@stepniewski.tech>
2021-12-25 20:00:58 +0100

    2 - application starts, but KafkaConumer is not stopped
    Now it works great! ...almost. Application starts, but I need to do one
    thing more. `KafkaConsumer` is never stop and it should be!

--- app/kafka/SampleKafkaConsumer.scala
+++ app/kafka/SampleKafkaConsumer.scala
@@ -6,16 +6,20 @@ import play.api.Logging
 
 import java.time.Duration
 import java.util.Properties
+import java.util.concurrent.Executors
 import javax.inject.Singleton
+import scala.concurrent.{ExecutionContext, Future}
 import scala.jdk.CollectionConverters.{IterableHasAsScala, SetHasAsJava}
-import scala.util.{Failure, Success, Try}
+import scala.util.{Failure, Success}
 
 @Singleton
 class SampleKafkaConsumer extends Logging {
 
   logger.info("Starting SampleKafkaConsumer")
 
-  private  val properties = new Properties()
+  val executionContext: ExecutionContext = ExecutionContext.fromExecutor(Executors.newSingleThreadExecutor())
+
+  private val properties = new Properties()
   properties.put("bootstrap.servers", "localhost:6003")
   properties.put("group.id", s"sample-group-id")
   properties.put("key.deserializer", classOf[StringDeserializer])
@@ -24,15 +28,15 @@ class SampleKafkaConsumer extends Logging {
   val kafkaConsumer = new KafkaConsumer[String, String](properties)
   kafkaConsumer.subscribe(Set("sample-topic").asJava)
 
-  Try {
+  Future {
     while (true) {
       kafkaConsumer.poll(Duration.ofSeconds(3)).asScala
         .foreach(r => {
           logger.info(s"SampleKafkaConsumer receive record $r")
         })
     }
-  } match {
+  }(executionContext).andThen {
     case Success(_) => logger.info(s"SampleKafkaConsumer succeed.")
     case Failure(e) => logger.error(s"SampleKafkaConsumer fail.", e)
-  }
+  }(executionContext)
 }
