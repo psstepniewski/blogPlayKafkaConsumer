76c58747df569a1ac76cf6fac98f2392003a2172
Pawel Stepniewski <pawel@stepniewski.tech>
2021-12-26 14:34:07 +0100

    3 - final version
    `KafkaConsumer` is not thread-safe, so I need to stop it in the same
    thread as `while(true)` loop runs. To do it I must add some quit condition.
    I will follow [kafka documentation](https://kafka.apache.org/26/javadoc/index.html?org/apache/kafka/clients/consumer/KafkaConsumer.html)
    and I will add `AtomicBoolean` to keep information if consumer should stop,
    with default `false` value. If `while(!stopConsumer.get())` loop will quit,
    `KafkaConsumer` will stop. Now I have to change `stopConsumer` flag value
    from `false` to `true`. For this type of needs Play Framework provides
    [CoordinatedShutdown](https://www.playframework.com/documentation/2.8.x/Shutdown) component
    
    I describe how to use plain `KafkaConsumer` in Play Framework.
    It always worth to know how lower-level components works, but after all
    I strongly encourage you to consider use libraries like `Alpakka`.

--- app/kafka/SampleKafkaConsumer.scala
+++ app/kafka/SampleKafkaConsumer.scala
@@ -1,5 +1,7 @@
 package kafka
 
+import akka.Done
+import akka.actor.CoordinatedShutdown
 import org.apache.kafka.clients.consumer.KafkaConsumer
 import org.apache.kafka.common.serialization.StringDeserializer
 import play.api.Logging
@@ -7,17 +9,19 @@ import play.api.Logging
 import java.time.Duration
 import java.util.Properties
 import java.util.concurrent.Executors
-import javax.inject.Singleton
+import java.util.concurrent.atomic.AtomicBoolean
+import javax.inject.{Inject, Singleton}
 import scala.concurrent.{ExecutionContext, Future}
 import scala.jdk.CollectionConverters.{IterableHasAsScala, SetHasAsJava}
 import scala.util.{Failure, Success}
 
 @Singleton
-class SampleKafkaConsumer extends Logging {
+class SampleKafkaConsumer @Inject()(coordinatedShutdown: CoordinatedShutdown) extends Logging {
 
-  logger.info("Starting SampleKafkaConsumer")
+  logger.info("SampleKafkaConsumer starts")
 
-  val executionContext: ExecutionContext = ExecutionContext.fromExecutor(Executors.newSingleThreadExecutor())
+  private val executionContext: ExecutionContext = ExecutionContext.fromExecutor(Executors.newSingleThreadExecutor())
+  private val stopConsumer: AtomicBoolean = new AtomicBoolean(false)
 
   private val properties = new Properties()
   properties.put("bootstrap.servers", "localhost:6003")
@@ -29,14 +33,28 @@ class SampleKafkaConsumer extends Logging {
   kafkaConsumer.subscribe(Set("sample-topic").asJava)
 
   Future {
-    while (true) {
-      kafkaConsumer.poll(Duration.ofSeconds(3)).asScala
+    while (!stopConsumer.get()) {
+      kafkaConsumer.poll(Duration.ofSeconds(60)).asScala
         .foreach(r => {
-          logger.info(s"SampleKafkaConsumer receive record $r")
+          logger.info(s"SampleKafkaConsumer receives record: $r")
         })
     }
-  }(executionContext).andThen {
-    case Success(_) => logger.info(s"SampleKafkaConsumer succeed.")
-    case Failure(e) => logger.error(s"SampleKafkaConsumer fail.", e)
+    logger.info(s"SampleKafkaConsumer quits 'while(true)' loop.")
   }(executionContext)
+  .andThen(_ => kafkaConsumer.close())(executionContext)
+  .andThen {
+    case Success(_) =>
+      kafkaConsumer.close()
+    case Failure(e) =>
+      kafkaConsumer.close()
+  }(executionContext)
+
+  coordinatedShutdown.addTask(CoordinatedShutdown.PhaseServiceStop, "SampleKafkaConsumer-stop"){() =>
+    logger.info("Shutdown-task[SampleKafkaConsumer-stop] starts.")
+    stopConsumer.set(true)
+    Future{ Done }(executionContext).andThen{
+      case Success(_) => logger.info("Shutdown-task[SampleKafkaConsumer-stop] succeed.")
+      case Failure(e) => logger.error("Shutdown-task[SampleKafkaConsumer-stop] fails.", e)
+    }(executionContext)
+  }
 }
